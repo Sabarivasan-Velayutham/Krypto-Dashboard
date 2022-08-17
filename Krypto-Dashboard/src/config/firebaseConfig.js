6494a2b9f32781d23770f3a32be5fea5e24c6e10
Sabarivasan Velayutham <84288319+Sabarivasan-Velayutham@users.noreply.github.com>
2022-08-17 15:40:24 +0530

    Add files via upload

---
+++ Krypto-Dashboard/src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjXAOgBQkNDuNE668xCva06Xiyth-eDX0",
+  authDomain: "sabari-krypto-dashboard.firebaseapp.com",
+  projectId: "sabari-krypto-dashboard",
+  storageBucket: "sabari-krypto-dashboard.appspot.com",
+  messagingSenderId: "776879592066",
+  appId: "1:776879592066:web:44799898587f0b8e3f5a9b"
+
+};
+
+export default firebaseConfig;
